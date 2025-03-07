e75aab5fa254e15f5db956738c624327ca0598eb
lucasbraga <lu.braga173@gmail.com>
2025-03-07 12:14:55 -0300

    ordenado por similaridade de nome de municipio para evitar bairros com nomes iguais ou parecidos tomar precedencia do real

--- CEP2CCS.py
+++ CEP2CCS.py
@@ -15,17 +15,25 @@ conn = psycopg2.connect(dbname=dbname, user=user, password=password, port=port, 
 # Recebendo o CEP
 
 cep = input('Digite o CEP: ')
-endereco = brazilcep.get_address_from_cep(cep)
+try: 
+    endereco = brazilcep.get_address_from_cep(cep)
+except:
+    raise Exception('CEP inválido')
+    
 un_ter = unidecode(endereco['district'])
 cidade = unidecode(endereco['city'])
+if cidade == 'Rio de Janeiro':
+    cidade = un_ter
 # Consultando os dados CEP no banco de dados
 
 cursor = conn.cursor()
 first_string = f"SELECT ccs.nome, cisp.id, cisp.aisp as aisp,cisp.risp as risp,cisp.un_ter as un_ter, ccs.status FROM ((cisp_relacao INNER JOIN cisp ON cisp.id = cisp_relacao.idcisp)  INNER JOIN ccs ON ccs.id = cisp_relacao.idccs) where ccs.nome like '%{cidade}%' "
-secondstr = f" or cisp.un_ter like '%{un_ter}%' limit 1"
+secondstr = f" or cisp.un_ter like '%{un_ter}%'"
 if (un_ter == 'centro' or un_ter == 'Centro') and cidade != 'Rio de Janeiro':
     un_ter = cidade
-cursor.execute(first_string + secondstr)
+final_str = f" order by ccs.nome <-> '{cidade}' limit 1"
+
+cursor.execute(first_string + secondstr + final_str)
 dado = cursor.fetchall()
 if dado:
         print(f'Conselho: {dado[0][0]}')
